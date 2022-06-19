8c44f487699eac13e767440d3c5fef1a96c6dc23
Ankur Prajapati <ankurp@geekyants.com>
2022-06-20 00:43:12 +0530

    added vibration

--- screens/ArcSlider/index.tsx
+++ screens/ArcSlider/index.tsx
@@ -1,16 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Vibration, View } from "react-native";
 import React, { useEffect, useRef, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Component from "./Component";
 
 const Index = () => {
+  const didMountRef = useRef(false);
   const [progress, setProgress] = useState(35);
+
+  const limit = 40;
+
+  const isAmountNotAllowed = progress > limit;
+
+  useEffect(() => {
+    //dont vibrate on first mount
+    if (didMountRef.current) {
+      Vibration.vibrate([0, 20]);
+    } else {
+      didMountRef.current = true;
+    }
+  }, [isAmountNotAllowed]);
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
         <Text style={{ marginBottom: 30 }}>{progress}</Text>
-        <Component progress={progress} limit={35} onChange={setProgress} />
+        <Component progress={progress} limit={limit} onChange={setProgress} />
       </View>
     </GestureHandlerRootView>
   );
